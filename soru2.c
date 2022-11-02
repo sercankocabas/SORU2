7d52ffeb52053e06fe38cf2bdbc736b859e056d1
sercankocabas <117014960+sercankocabas@users.noreply.github.com>
2022-11-02 17:06:19 +0300

    Add files via upload

---
+++ soru2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+int f,x,y;
+int toplam,cozum;
+int fark;
+int sonuc(int x){
+	for(y=1;y<=100;y++){
+		cozum+=y;
+		toplam=toplam+y*y;
+	}
+	printf("toplamin karesi: %d",cozum*cozum);
+    printf("kareleri toplami: %d",toplam);
+    
+}
+int main(){
+	
+	printf("girin: ");
+	scanf("%d",&x);
+	sonuc(x);
+	fark=cozum*cozum-toplam;
+	printf("fark: %d",fark);
+		
+	return 0;
+}
